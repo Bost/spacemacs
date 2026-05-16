c0c5009629dc7a9173bd3d52e2f84a77578168e9
Rostislav Svoboda <Rostislav.Svoboda@posteo.net>
2026-05-16 13:35:13 +0200

    spacemacs-org/config.el no-byte-compile
    Needed for `guix build --with-input=emacs-minimal=emacs emacs-spacemacs`

--- layers/+spacemacs/spacemacs-org/config.el
+++ layers/+spacemacs/spacemacs-org/config.el
@@ -1,3 +1,4 @@
+;; -*- no-byte-compile: t -*-
 ;;; config.el --- spacemacs-org layer configuration file for Spacemacs.  -*- lexical-binding: nil; -*-
 ;;
 ;; Copyright (c) 2012-2025 Sylvain Benner & Contributors
